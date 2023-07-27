b0d967c3b100de47fa13ee4dc0bf8d8321ec45cb
Dear.Va <1328886154@qq.com>
2023-07-27 15:37:21 +0800

    Create AsyncExtension.cs

---
+++ Extensions/AsyncExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Antelcat.Wpf.Extensions;
+
+public static class AsyncExtension {
+    /// <summary>
+    /// 等待按钮点击
+    /// </summary>
+    /// <param name="button"></param>
+    /// <returns></returns>
+    public static void WaitForClick(this Button button) {
+        var frame = new DispatcherFrame();
+
+        void ContinueFrame(object? sender, object? args) {
+            button.Unloaded -= ContinueFrame;
+            button.Click -= ContinueFrame;
+            frame.Continue = false;
+        }
+
+        button.Unloaded += ContinueFrame;
+        button.Click += ContinueFrame;
+        Dispatcher.PushFrame(frame);
+    }
+}
