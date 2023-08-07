9ac07651614a07a65bad0d527cd363bb400e1878
Dear.Va <1328886154@qq.com>
2023-08-07 16:00:22 +0800

    依赖注入动态控件

---
+++ Controls/DynamicControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using Antelcat.Wpf.Models;
+
+namespace Antelcat.Wpf.Controls;
+
+/// <summary>
+/// 提供可以依赖注入的动态控件
+/// </summary>
+public class DynamicControl : ContentControl, INameScope {
+    public static readonly DependencyProperty DynamicContextProperty = DependencyProperty.Register(
+        nameof(DynamicContext), typeof(DynamicContext), typeof(DynamicControl), 
+        new PropertyMetadata(default(DynamicContext?)));
+
+    public DynamicContext? DynamicContext {
+        get => (DynamicContext?)GetValue(DynamicContextProperty);
+        set => SetValue(DynamicContextProperty, value);
+    }
+
+    protected override void OnContentChanged(object oldContent, object newContent) {
+        if (DynamicContext == null || newContent is not FrameworkElement element) {
+            return;
+        }
+
+        var newType = newContent.GetType();
+        foreach (var binding in DynamicContext.Bindings) {
+            var propertyName = binding.Property ??
+                               throw new NullReferenceException(nameof(binding.Property));
+            var property = newType.GetField(
+                propertyName + "Property",
+                BindingFlags.Static | BindingFlags.Public);
+            if (property == null) continue;
+            var dependencyProperty = property.GetValue(null) as DependencyProperty ??
+                                     throw new NullReferenceException(property.Name);
+            if (binding.Binding != null) {
+                element.SetBinding(
+                    dependencyProperty,
+                    binding.Binding);
+            } else {
+                element.SetValue(dependencyProperty, binding.Value);
+            }
+        }
+
+        base.OnContentChanged(oldContent, newContent);
+    }
+}
---
+++ Models/DynamicBinding.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Antelcat.Wpf.Models;
+
+[Localizability(LocalizationCategory.Ignore)]
+[DictionaryKeyProperty(nameof(TargetType))]
+[ContentProperty(nameof(Bindings))]
+public class DynamicContext : DependencyObject, IAddChild {
+    public Type TargetType {
+        get {
+            VerifyAccess();
+            return targetType;
+        }
+
+        set {
+            VerifyAccess();
+            
+            if (value == null) {
+                throw new ArgumentNullException(nameof(value));
+            }
+            
+            if (value == targetType) {
+                return;
+            }
+            
+            targetType = value;
+            nameScope.Clear();
+            foreach (var propertyInfo in targetType.GetProperties()) {
+                if (propertyInfo.CanWrite) {
+                    nameScope.RegisterName(propertyInfo.Name, propertyInfo);
+                }
+            }
+        }
+    }
+    
+    private Type targetType = typeof(object);
+    
+    /// <summary>
+    ///     The collection of property setters for the target type
+    /// </summary>
+    [DependsOn(nameof(TargetType))]
+    public Collection<DynamicBinding> Bindings {
+        get {
+            VerifyAccess();
+            return bindings ??= new Collection<DynamicBinding>();
+        }
+    }
+
+    private Collection<DynamicBinding>? bindings;
+    
+    private readonly NameScope nameScope = new();
+
+    public DynamicContext() {
+        NameScope.SetNameScope(this, nameScope);
+    }
+    
+    public void AddChild(object value) {
+        if (value is not DynamicBinding binding) {
+            throw new ArgumentException("value must be a DynamicBinding");
+        }
+        
+        Bindings.Add(binding);
+    }
+    
+    public void AddText(string text) {
+        throw new NotSupportedException();
+    }
+}
+
+public class DynamicBinding : DependencyObject {
+    [Ambient] public string? Property { get; set; }
+
+    [DependsOn(nameof(Property))] public object? Value { get; set; }
+
+    [DependsOn(nameof(Property))] public BindingBase? Binding { get; set; }
+}
