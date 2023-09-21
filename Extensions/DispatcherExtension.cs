59dca3b717b7323b10afdfb9d54c0003417f9b8f
Feast <feast107@gmail.com>
2023-09-21 17:21:58 +0800

    Update DispatcherExtension.cs

--- Extensions/DispatcherExtension.cs
+++ Extensions/DispatcherExtension.cs
@@ -7,40 +7,57 @@ namespace Antelcat.Wpf.Extensions;
 public static class DispatcherExtension
 {
 	/// <summary>
-	/// 在指定的Dispatcher上运行一个Task，等待其完成，避免阻塞UI线程
+	/// 在指定的DispatcherFrame上运行一个Task，等待其完成，避免阻塞UI线程
 	/// </summary>
-	/// <param name="dispatcher"></param>
+	/// <param name="task"></param>
+	/// <returns></returns>
+	/// <exception cref="AggregateException"></exception>
+	public static void AwaitOnDispatcherFrame(this Task task)
+	{
+		var                 frame             = new DispatcherFrame();
+		AggregateException? capturedException = null;
+		
+		task.ContinueWith(t =>
+		{
+			capturedException = t.Exception;
+			frame.Continue    = false; // 结束消息循环
+		}, TaskContinuationOptions.AttachedToParent);
+		
+		Dispatcher.PushFrame(frame);
+		
+		if (capturedException != null)
+		{
+			throw capturedException;
+		}
+	}	
+	
+	/// <summary>
+	/// 在指定的DispatcherFrame上运行一个Task，等待其完成，避免阻塞UI线程
+	/// </summary>
 	/// <param name="task"></param>
 	/// <typeparam name="TResult"></typeparam>
 	/// <returns></returns>
 	/// <exception cref="AggregateException"></exception>
-	public static TResult? RunTask<TResult>(this Dispatcher dispatcher, Task<TResult> task)
+	public static TResult? AwaitOnDispatcherFrame<TResult>(this Task<TResult> task)
 	{
 		var frame = new DispatcherFrame();
-		TResult? result = default;
-		Exception? capturedException = null;
 		
-		dispatcher.BeginInvoke(async () =>
+		TResult? result = default;
+		
+		AggregateException? capturedException = null;
+		
+		task.ContinueWith(t =>
 		{
-			try
-			{
-				result = await task;
-			}
-			catch (Exception e)
-			{
-				capturedException = e;
-			}
-			finally
-			{
-				frame.Continue = false; // 结束消息循环
-			}
-		});
+			capturedException = t.Exception;
+			result            = t.Result;
+			frame.Continue    = false; // 结束消息循环
+		}, TaskContinuationOptions.AttachedToParent);
 		
 		Dispatcher.PushFrame(frame);
 		
-		if (capturedException is not null)
+		if (capturedException != null)
 		{
-			throw new AggregateException(capturedException);
+			throw capturedException;
 		}
 		
 		return result;
