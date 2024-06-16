213e6c0e8752c3d6d1f612a21469f1a72bff958b
Dear.Va <dear.va.love@gmail.com>
2024-06-16 17:34:26 +0800

    似乎没用

---
+++ Antelcat.Wpf/Utils/EmptyWindowAutomationPeer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Automation.Peers;
+
+namespace Antelcat.Wpf.Utils;
+
+public class EmptyWindowAutomationPeer(FrameworkElement owner) : FrameworkElementAutomationPeer(owner)
+{
+    protected override string GetNameCore() => nameof(EmptyWindowAutomationPeer);
+
+    protected override AutomationControlType GetAutomationControlTypeCore() => AutomationControlType.Window;
+
+    protected override List<AutomationPeer> GetChildrenCore() => [];
+}
