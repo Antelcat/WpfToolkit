d0efc0d600438ab0daabbf8622317d447c3559df
Dear.Va <dear.va.love@gmail.com>
2024-12-16 17:28:11 +0800

    add more adorners

--- Antelcat.Wpf/Attachments/AdornerAttach.cs
+++ Antelcat.Wpf/Attachments/AdornerAttach.cs
@@ -1,7 +1,7 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Documents;
-using System.Windows.Media;
+using Antelcat.Wpf.Controls;
 
 namespace Antelcat.Wpf.Attachments;
 
@@ -14,57 +14,35 @@ public class AdornerAttach
 
     public static void SetContent(DependencyObject d, object? value) => d.SetValue(ContentProperty, value);
 
-    // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty ContentProperty =
         DependencyProperty.RegisterAttached("Content",
             typeof(object),
             typeof(AdornerAttach),
             new PropertyMetadata(null, ContentProperty_OnChanged));
+    
+    private readonly static Dictionary<UIElement, Adorner> AttachedAdorners = new();
 
     private static void ContentProperty_OnChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is not UIElement uiElement) return;
         var adornerLayer = AdornerLayer.GetAdornerLayer(uiElement);
         if (adornerLayer == null) return;
-        if (adornerLayer.GetAdorners(uiElement) is { } adorners)
+        
+        if (e.OldValue != null && AttachedAdorners.TryGetValue(uiElement, out var adorner))
         {
-            foreach (var adorner in adorners)
-            {
-                if (adorner is SimpleAdorner)
-                {
-                    adornerLayer.Remove(adorner);
-                }
-            }
+            adornerLayer.Remove(adorner);
+            AttachedAdorners.Remove(uiElement);
         }
 
         if (e.NewValue != null)
         {
-            var adorner = new SimpleAdorner(uiElement, e.NewValue);
+            adorner = e.NewValue switch
+            {
+                UIElement element => new UIElementAdorner(uiElement, element),
+                _ => new ContentPresenterAdorner(uiElement, e.NewValue)
+            };
             adornerLayer.Add(adorner);
-        }
-    }
-
-    private class SimpleAdorner : Adorner
-    {
-        private readonly ContentPresenter contentPresenter = new();
-        
-        public SimpleAdorner(UIElement adornedElement, object content) : base(adornedElement)
-        {
-            contentPresenter.Content = content;
-            AddVisualChild(contentPresenter);
-        }
-
-        protected override Visual GetVisualChild(int index)
-        {
-            return contentPresenter;
-        }
-
-        protected override int VisualChildrenCount => 1;
-
-        protected override Size ArrangeOverride(Size finalSize)
-        {
-            contentPresenter.Arrange(new Rect(new Point(0, 0), finalSize));
-            return finalSize;
+            AttachedAdorners.Add(uiElement, adorner);
         }
     }
 }
---
+++ Antelcat.Wpf/Controls/Adorners.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Antelcat.Wpf.Controls;
+
+public class VisualAdorner(UIElement adornedElement, Visual visual) : Adorner(adornedElement)
+{
+    protected override int VisualChildrenCount => 1;
+
+    protected override Visual GetVisualChild(int index) => visual;
+}
+
+public class UIElementAdorner(UIElement adornedElement, UIElement uiElement) : VisualAdorner(adornedElement, uiElement)
+{
+    protected override Size ArrangeOverride(Size finalSize)
+    {
+        uiElement.Arrange(new Rect(finalSize));
+        return finalSize;
+    }
+
+    protected override Size MeasureOverride(Size constraint)
+    {
+        uiElement.Measure(constraint);
+        return uiElement.DesiredSize;
+    }
+}
+
+public class ContentPresenterAdorner(UIElement adornedElement, object content)
+    : UIElementAdorner(adornedElement, new ContentPresenter { Content = content });
